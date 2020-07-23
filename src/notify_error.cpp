781cb11b99f73ffec6fd03c22bc2901d3376c504
Phoomparin Mano <phoomparin@gmail.com>
2020-07-24 01:32:41 +0700

    fix: do not use flash led to notify error because it hurts my eyes

--- src/notify_error.cpp
+++ src/notify_error.cpp
@@ -3,28 +3,24 @@
 #include "../lib/config.h"
 #include "../lib/leds.h"
 
+// Notify the human operator that an unrecoverable error exists.
+// This will last 5 seconds.
+void notify_critical_error() {
+  // Some LED to grab human attention that something is wrong.
+
+  for (int i = 0; i < 5; i++) {
+    led_on();
+    delay(500);
+
+    led_off();
+    delay(500);
+  }
+}
+
 // Notify the human operator then restart the device.
 void panic() {
   notify_critical_error();
   esp_restart();
-}
-
-// Notify the human operator that an unrecoverable error exists.
-// This will last 5 seconds.
-void notify_critical_error() {
-  // Some LED and flash to grab human attention that something is wrong.
-
-  for (int i = 0; i < 5; i++) {
-    led_on();
-    flash_on();
-
-    delay(500);
-
-    led_off();
-    flash_off();
-
-    delay(500);
-  }
 }
 
 // Notify the human operator that something is not right, but not critical.
